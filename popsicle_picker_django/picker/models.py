bccba00fbaf0083c8dc3af089cb80e9844015437
Lexi <lexi.gude@gmail.com>
2023-03-25 16:55:49 -0400

    Renames object_builder to object_manager; adds alt_approved field to Pop model; adds beginnings of alt_text user submission; minor tweaks

--- popsicle_picker_django/picker/models.py
+++ popsicle_picker_django/picker/models.py
@@ -1,21 +1,18 @@
 from django.db import models
 
-# Create your models here.
 
-class Pop(models.model):
-    name = models.CharField
-    image = models.ImageField
-    image_alt = models.CharField
+class Pop(models.Model):
+    name = models.CharField(max_length=20)
+    image = models.CharField(max_length=300)
+    image_alt = models.CharField(max_length=200)
+    alt_approved = models.BooleanField(default=False)
     favorites_count = models.IntegerField(default=0)
     # figure out how to check if dairy
     # dairy = models.BooleanField
 
 
-class User(models.model):
+class User(models.Model):
     username = models.CharField(max_length=15)
-    # favorites = models.list of foreign keys ? 
-    # dislikes = models.list of foreign keys ?
-    dairy_free = models.BooleanField
-    incl_choc_cover = models.BooleanField  
-    # will probably need:
-    email = models.EmailField  
+    dairy_free = models.BooleanField()
+    incl_choc_cover = models.BooleanField()
+    email = models.EmailField()
--- popsicle_picker_django/picker/views.py
+++ popsicle_picker_django/picker/views.py
@@ -2,12 +2,12 @@ from picker.picker import get_pop_pack
 from django.shortcuts import render
 from django.template import loader
 
-# Create your views here.
+
 def picker(request):
     return render(request, 'picker/main.html')
 
 def result(request):
     # pop = get_pop_pack()
     # for testing:
-    pop = {'image': '/static/picker/test_image.jpg', 'name': 'Toasted Buckeye: 3-tiered'}
+    pop = {'image': '/static/picker/test_image.jpg', 'name': 'Toasted Buckeye: 3-tiered', 'image_alt': ''}
     return render(request, 'picker/result.html', pop)
